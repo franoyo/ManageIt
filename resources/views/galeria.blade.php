54237646a0011842c4a307bdecfb75062c6b1a2b
franoyo <127349021+franoyo@users.noreply.github.com>
2024-09-12 12:50:52 -0500

    Update galeria.blade.php

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/styleGaleria.css?v=1.31') }}">
+    <link rel="stylesheet" href="{{ asset('css/styleGaleria.css?v=1.311') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
